c1bc363100778f02c1da323eb537753533098745
Jennifer Cadiz <jen_cadiz@hotmail.com>
2020-02-26 00:17:10 -0400

    Varios diseño

--- assets/js/prueba.js
+++ assets/js/prueba.js
@@ -1,12 +1,44 @@
-$(document).ready(function(){
-	$(".dropdown").hover(            
-			function() {
-					$('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideDown("fast");
-					$(this).toggleClass('open');        
-			},
-			function() {
-					$('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideUp("fast");
-					$(this).toggleClass('open');       
-			}
+$(document).ready(function () {
+	$(".dropdown").hover(
+		function () {
+			$('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideDown("fast");
+			$(this).toggleClass('open');
+		},
+		function () {
+			$('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideUp("fast");
+			$(this).toggleClass('open');
+		}
 	);
+	// quantity change js
+	$('.pro-qty').prepend('<span class="dec qtybtn">-</span>');
+	$('.pro-qty').append('<span class="inc qtybtn">+</span>');
+	$('.qtybtn').on('click', function () {
+		var $button = $(this);
+		var oldValue = $button.parent().find('input').val();
+		if ($button.hasClass('inc')) {
+			var newVal = parseFloat(oldValue) + 1;
+		} else {
+			// Don't allow decrementing below zero
+			if (oldValue > 0) {
+				var newVal = parseFloat(oldValue) - 1;
+			} else {
+				newVal = 0;
+			}
+		}
+		$button.parent().find('input').val(newVal);
+	});
+
+	$(window).on('scroll', function () {
+		if ($(this).scrollTop() > 600) {
+			$('.scroll-top').removeClass('not-visible d-none');
+		} else {
+			$('.scroll-top').addClass('not-visible');
+		}
+	});
+	$('.scroll-top').on('click', function (event) {
+		$('html,body').animate({
+			scrollTop: 0
+		}, 1000);
+	});
+	
 });
