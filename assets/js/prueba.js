96445507adc90d2bd4634329965e3f38af7fe329
Jennifer Cadiz <jen_cadiz@hotmail.com>
2020-04-05 23:25:28 -0400

    Arreglo de vistas

--- assets/js/prueba.js
+++ assets/js/prueba.js
@@ -9,10 +9,10 @@ $(document).ready(function () {
 			$(this).toggleClass('open');
 		}
 	);
-	// quantity change js
-	$('.pro-qty').prepend('<span class="dec qtybtn">-</span>');
-	$('.pro-qty').append('<span class="inc qtybtn">+</span>');
-	$('.qtybtn').on('click', function () {
+	// product-detail-quantity change js
+	$('.quantitycontrol').prepend('<span class="dec quantity-button"><i class="fas fa-minus"></i></span>');
+	$('.quantitycontrol').append('<span class="inc quantity-button"><i class="fas fa-plus"></i></span>');
+	$('.quantity-button').on('click', function () {
 		var $button = $(this);
 		var oldValue = $button.parent().find('input').val();
 		if ($button.hasClass('inc')) {
@@ -29,7 +29,7 @@ $(document).ready(function () {
 	});
 
 	$(window).on('scroll', function () {
-		if ($(this).scrollTop() > 1100) {
+		if ($(this).scrollTop() > 800) {
 			$('.scroll-top').removeClass('not-visible d-none');
 		} else {
 			$('.scroll-top').addClass('not-visible');
@@ -54,4 +54,34 @@ $(document).ready(function () {
 
 	});
 
+});
+
+$('input[name=\'shipping_address\']').on('change', function () {
+	if (this.value == 'new') {
+		$('#shipping-existing').hide();
+		$('#shipping-new').show();
+	} else {
+		$('#shipping-existing').show();
+		$('#shipping-new').hide();
+	}
+});
+
+$('#collapse-shipping-address .form-group[data-sort]').detach().each(function () {
+	if ($(this).attr('data-sort') >= 0 && $(this).attr('data-sort') <= $(
+			'#collapse-shipping-address .form-group').length - 2) {
+		$('#collapse-shipping-address .form-group').eq(parseInt($(this).attr('data-sort')) + 2)
+			.before(this);
+	}
+
+	if ($(this).attr('data-sort') > $('#collapse-shipping-address .form-group').length - 2) {
+		$('#collapse-shipping-address .form-group:last').after(this);
+	}
+
+	if ($(this).attr('data-sort') == $('#collapse-shipping-address .form-group').length - 2) {
+		$('#collapse-shipping-address .form-group:last').after(this);
+	}
+
+	if ($(this).attr('data-sort') < -$('#collapse-shipping-address .form-group').length - 2) {
+		$('#collapse-shipping-address .form-group:first').before(this);
+	}
 });
